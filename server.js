3adc680cfebb1bf07fbaad737e90e37f2289882c
pr0grara <azbaghda@gmail.com>
2021-01-05 15:51:42 -0800

    initial commit

---
+++ server.js
@@ -0,0 +1,17 @@
+import express from "express";
+import mongoose from "mongoose";
+
+const PORT = process.env.HTTP_PORT || 4001;
+const app = express();
+app.get("/", (req, res) => {
+  res.send("just gonna send it");
+});
+app.get("/flower", (req, res) => {
+  res.json({
+    name: "Dandelion",
+    colour: "Blue-ish",
+  });
+});
+app.listen(PORT, () => {
+  console.log(`Server listening at port ${PORT}.`);
+});
